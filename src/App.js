07dbd7898d2b3fe723a2949745c4c4b5fe9fcc23
Bruna Loyola <brunloyola02@gmail.com>
2023-03-14 07:32:14 -0300

    Requisito 02

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Header from './components /Header';
 import Album from './pages/Album';
 import Favorites from './pages/Favorites';
 import Login from './pages/Login';
@@ -12,6 +13,7 @@ class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
+        <Route exact path="/header" component={ Header } />
         <Switch>
           <Route exact path="/" component={ Login } />
           <Route path="/search" component={ Search } />
@@ -20,6 +22,9 @@ class App extends React.Component {
           <Route exact path="/profile" component={ Profile } />
           <Route path="/profile/edit" component={ ProfileEdit } />
           <Route path="*" component={ NotFound } />
+          {/* <Route exact path="/">
+            {logado ? <Redirect to="/dashboard" /> : <Login />}
+          </Route> */}
         </Switch>
       </BrowserRouter>
     );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,83 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Message from '../components /Message';
+import { createUser } from '../services/userAPI';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      user: { name: '',
+        email: '',
+        image: '',
+        description: '' },
+      isDisabled: true,
+      logado: false,
+
+    };
+  }
+
+  validacao = () => {
+    const { user } = this.state;
+    const valorTeste = 3;
+    if (user.name.length < valorTeste) {
+      return this.setState({ isDisabled: true });
+    }
+    this.setState({ isDisabled: false });
+  };
+
+  onInputChange = ({ target }) => {
+    const { value } = target;
+    this.setState({
+      user: { name: value },
+    }, this.validacao);
+  };
+
+  onSaveButtonClick = async () => {
+    const {
+      user,
+    } = this.state;
+
+    this.setState({ logado: true }, async () => {
+      await createUser(user);
+      const { history } = this.props;
+      history.push('/search', this.state);
+    });
+  };
+
   render() {
-    return (<div data-testid="page-login" />);
+    const { isDisabled, logado } = this.state;
+    return (
+      <div>
+        {
+          logado ? <Message />
+            : (
+              <div data-testid="page-login">
+                <label htmlFor="">
+                  <input
+                    type="text"
+                    data-testid="login-name-input"
+                    onChange={ this.onInputChange }
+                  />
+                </label>
+                <button
+                  data-testid="login-submit-button"
+                  disabled={ isDisabled }
+                  onClick={ this.onSaveButtonClick }
+                >
+                  Entrar
+
+                </button>
+              </div>
+            )
+        }
+      </div>
+    );
   }
 }
 
 export default Login;
+
+Login.propTypes = {
+  history: PropTypes.shape().isRequired,
+};
